131ac368e7447f243f6ea66e4d79c312e226df69
Mohamed Bouzargtoun <mobouzar@e2r10p4.1337.ma>
2019-05-27 00:21:04 +0000

    final-version

--- checker.c
+++ checker.c
@@ -1,18 +1,46 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Checker.c                                          :+:      :+:    :+:   */
+/*   checker.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mobouzar <mobouzar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/05/11 01:51:07 by mobouzar          #+#    #+#             */
-/*   Updated: 2019/05/12 22:52:41 by mobouzar         ###   ########.fr       */
+/*   Created: 2019/05/18 00:39:23 by mobouzar          #+#    #+#             */
+/*   Updated: 2019/05/25 21:54:04 by mobouzar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-int		is_square_valid(char **tetrimino)
+static int	read_file(int fd, t_tetrimino *tetri)
+{
+	char	buff[22];
+	char	*temp;
+	int		rd;
+	int		i;
+	int		count;
+
+	i = -1;
+	count = 0;
+	if ((rd = read(fd, buff, 21)) != 21)
+		buff[rd] = '\0';
+	while (buff[++i])
+		if (buff[i] == '\n')
+			count++;
+	i = 0;
+	while (i < 4 && ((rd == 20 && count == 4) || (rd == 21 && count == 5)))
+	{
+		temp = ft_strsub(buff, i * 5, 4);
+		ft_strcpy(tetri->tetrimino[i], temp);
+		ft_strdel(&temp);
+		i++;
+	}
+	if (rd == 0)
+		return (-1);
+	return ((rd == 21 || rd == 20) ? 1 : 0);
+}
+
+static int	is_square_valid(char tetrimino[4][5])
 {
 	int		i;
 	int		j;
@@ -36,7 +64,7 @@ int		is_square_valid(char **tetrimino)
 	return ((hashtag == 4 && point == 12) ? 1 : 0);
 }
 
-int		is_tetrimino_valid(char **tetrimino)
+static int	is_tetrimino_valid(char tetrimino[4][5])
 {
 	int		i;
 	int		j;
@@ -53,13 +81,35 @@ int		is_tetrimino_valid(char **tetrimino)
 			{
 				if (tetrimino[i][j] == '#')
 				{
-					(tetrimino[i++][j] == '#') ? count++ : count;
-					(tetrimino[i--][j] == '#') ? count++ : count;
-					(tetrimino[i][j++] == '#') ? count++ : count;
-					(tetrimino[i][j--] == '#') ? count++ : count;
+					(i < 3 && tetrimino[i + 1][j] == '#') ? count++ : count;
+					(i > 0 && tetrimino[i - 1][j] == '#') ? count++ : count;
+					(tetrimino[i][j + 1] == '#') ? count++ : count;
+					(j > 0 && tetrimino[i][j - 1] == '#') ? count++ : count;
 				}
 			}
 		}
 	}
 	return ((count == 6 || count == 8) ? 1 : 0);
+}
+
+int			get_tetriminos(int fd, t_tetriminos *t)
+{
+	int check;
+
+	t->nbt = 0;
+	while ((check = read_file(fd, &t->tetriminos[t->nbt])))
+	{
+		if (check == -1)
+			return (1);
+		if (is_tetrimino_valid(t->tetriminos[t->nbt].tetrimino))
+		{
+			drag_tetrimino(&t->tetriminos[t->nbt]);
+			t->nbt++;
+		}
+		else
+			return (0);
+	}
+	if (check == 0)
+		return (0);
+	return (1);
 }
--- fillit.h
+++ fillit.h
@@ -6,28 +6,41 @@
 /*   By: mobouzar <mobouzar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/11 01:51:35 by mobouzar          #+#    #+#             */
-/*   Updated: 2019/05/12 19:17:37 by mobouzar         ###   ########.fr       */
+/*   Updated: 2019/05/25 21:54:07 by mobouzar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FILLIT_H
-#define FILLIT_H
+# define FILLIT_H
 
-# include "get_next_line/get_next_line.h"
-# include "get_next_line/libft/libft.h"
+# include "libft/libft.h"
 # include <fcntl.h>
-# include <string.h>
 
-typedef struct  s_points
+typedef struct	s_points
 {
-        int     x;
-        int     y;
-}               t_points;
+	int			x;
+	int			y;
+}				t_points;
 
-typedef struct  s_tetriminos
+typedef struct	s_tetrimino
 {
-        char    tetrimino[4][5];
-}               t_tetriminos;
+	char		tetrimino[4][5];
+	t_points	hashtag[4];
+}				t_tetrimino;
 
+typedef struct	s_tetriminos
+{
+	t_tetrimino	tetriminos[26];
+	int			nbt;
+	char		map[11][11];
+	int			len;
+}				t_tetriminos;
+
+int				get_tetriminos(int fd, t_tetriminos *t);
+int				set_tetrimino(t_tetriminos *t, int tetpos, t_points p);
+void			drag_tetrimino(t_tetrimino *t);
+int				delete_tetrimino(t_tetriminos *t, int tetpos, t_points p);
+int				solve(t_tetriminos *t, int i);
+void			display(t_tetriminos t);
 
 #endif
